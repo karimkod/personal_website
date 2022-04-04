faff04435e332c9c32e209c1ca9c81a718ab0bf1
Abdelkrim Bournane <abdelkrim.bournane@gmail.com>
2022-04-04 13:11:58 +0200

    Ignore unwanted files

--- src/PersonalWebsite.Application/Profile/Query/GetProfileHandler.cs
+++ src/PersonalWebsite.Application/Profile/Query/GetProfileHandler.cs
@@ -1,9 +1,17 @@
-﻿namespace PersonalWebsite.Application.Profile.Query;
+﻿using System;
+
+namespace PersonalWebsite.Application.Profile.Query;
 
 public class GetProfileHandler
 {
     public GetProfileResponse Handle(GetProfile getProfileCommand)
     {
-        return null;
+        return new GetProfileResponse()
+        {
+            FirstName = "firstName",
+            LastName = "lastName",
+            Birthdate = new DateTime(1995, 11, 25), 
+            Bio = "This is my bio"
+        };
     }
 }
--- src/PersonalWebsite.Application/Profile/Query/GetProfileResponse.cs
+++ src/PersonalWebsite.Application/Profile/Query/GetProfileResponse.cs
@@ -2,11 +2,42 @@
 
 namespace PersonalWebsite.Application.Profile.Query;
 
-public class GetProfileResponse
+public class GetProfileResponse : IEquatable<GetProfileResponse>
 {
+
     public string LastName { get; set; }
-    public string  FirstName { get; set; }
+    public string FirstName { get; set; }
     public DateTime Birthdate { get; set; }
     public string Bio { get; set; }
     
+    public bool Equals(GetProfileResponse? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return LastName == other.LastName && FirstName == other.FirstName && Birthdate.Equals(other.Birthdate) &&
+               Bio == other.Bio;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((GetProfileResponse) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(LastName, FirstName, Birthdate, Bio);
+    }
+
+    public static bool operator ==(GetProfileResponse? left, GetProfileResponse? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(GetProfileResponse? left, GetProfileResponse? right)
+    {
+        return !Equals(left, right);
+    }
 }
